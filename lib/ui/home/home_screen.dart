43bc8a7d935e10520d95ad31223562f30093e466
Ferreyra Enzo <ferreyraenzo26@hotmail.com>
2025-11-13 10:52:25 -0300

    Refactor folder

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'perfil_screen.dart';
+import '../perfi/perfil_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
