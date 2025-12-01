2c6d5722676cfda2a3459475ef86c21a8090ec0e
Ferreyra Enzo <ferreyraenzo26@hotmail.com>
2025-12-01 14:14:13 -0300

    Secccion de privacidad y terminos

--- lib/ui/configuracion/config_screen.dart
+++ lib/ui/configuracion/config_screen.dart
@@ -4,6 +4,7 @@ import 'package:reciclapp/ui/configuracion/widgets/app_info_section.dart';
 // Secciones
 import 'package:reciclapp/ui/configuracion/widgets/appearance_section.dart';
 import 'package:reciclapp/ui/configuracion/widgets/language_section.dart';
+import 'package:reciclapp/ui/configuracion/widgets/legal_section.dart';
 import 'package:reciclapp/ui/configuracion/widgets/notifications_section.dart';
 
 class ConfigScreen extends StatelessWidget {
@@ -33,6 +34,9 @@ class ConfigScreen extends StatelessWidget {
           AppearanceConfigSection(),
           SizedBox(height: 16),
           AppInfoSection(),
+          SizedBox(height: 16),
+          LegalConfigSection(),
+
 
         ],
       ),
